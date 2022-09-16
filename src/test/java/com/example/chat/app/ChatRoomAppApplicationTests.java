de3dc16518636a3eca2bcff57826e111591f5124
akash@22 <100702414+neekhara22@users.noreply.github.com>
2022-09-17 02:57:40 +0530

    Add files via upload
    real-time chat room application using Spring boot & Web Socket

---
+++ src/test/java/com/example/chat/app/ChatRoomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chat.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatRoomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
